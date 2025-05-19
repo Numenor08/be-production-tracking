2f6721a0d9e858f4f9bc9d8d1199bded2a7c85ca
Athaya Razan <142677331+Numenor08@users.noreply.github.com>
2025-05-19 16:27:23 +0700

    refact: clean some code

--- src/routes/salesOrder.route.ts
+++ src/routes/salesOrder.route.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import SalesOrderController from '../controllers/salesOrder.controller';
+import express from 'express'
+import SalesOrderController from '../controllers/salesOrder.controller'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', SalesOrderController.getSalesOrders);
-router.get('/:id', SalesOrderController.getSalesOrderById);
-router.post('/', SalesOrderController.createSalesOrder);
-router.put('/:id', SalesOrderController.updateSalesOrder);
-router.delete('/:id', SalesOrderController.deleteSalesOrder);
+router.get('/', SalesOrderController.getSalesOrders)
+router.get('/:id', SalesOrderController.getSalesOrderById)
+router.post('/', SalesOrderController.createSalesOrder)
+router.put('/:id', SalesOrderController.updateSalesOrder)
+router.delete('/:id', SalesOrderController.deleteSalesOrder)
 
-export default router;
+export default router
